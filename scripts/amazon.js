c236fc83fbcf941b231977a1039a8a5d4501cf16
DhruviMistry <damistry55@gmail.com>
2026-08-06 15:38:14 +0530

    Completed L14 Excercises

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
 //data structure for products - called data structure because it is a collection of data
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -58,19 +58,14 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-}
+const cartQuantity = calculateCartQuantity();
+document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
     addToCart(productId);
-    updateCartQuantity();
+    const cartQuantity = calculateCartQuantity();
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
