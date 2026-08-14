17f0a64ac567973e8691153effff5a6541ce33bf
DhruviMistry <damistry55@gmail.com>
2026-08-14 15:47:41 +0530

    Display Added message using setTimeout and make quantity selector intreactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -31,7 +31,7 @@ function renderProductsGrid() {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -49,7 +49,7 @@ function renderProductsGrid() {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -69,7 +69,31 @@ function renderProductsGrid() {
   document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
       const productId = button.dataset.productId;
-      addToCart(productId);
+
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`,
+      );
+      const quantity = Number(quantitySelector.value);
+
+      addToCart(productId, quantity);
+
+      const addedMessageTimeouts = {};
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`,
+      );
+      addedMessage.classList.add("added-to-cart-visible");
+
+      const prviousTimeoutId = addedMessageTimeouts[productId];
+      if (prviousTimeoutId) {
+        clearTimeout(prviousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove("added-to-cart-visible");
+      }, 2000);
+
+      addedMessageTimeouts[productId] = timeoutId;
+
       const cartQuantity = calculateCartQuantity();
       document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
     });
